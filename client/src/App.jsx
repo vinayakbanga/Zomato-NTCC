0e8d640a2f1db918dcce71d8c999832162220b60
Vinayak Banga <vinayakbanga22@gmail.com>
2022-06-07 01:08:56 +0530

    tailwind added

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+         <h1 className="text-3xl text-blue-700 font-bold underline">
+      Hello world!
+    </h1>
+      
+    </div>
+  );
+}
+
+export default App;
