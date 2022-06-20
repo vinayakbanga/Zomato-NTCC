337d46c2c2ea1194b2244e6d0e9cde53dc4a1c58
vinayakbanga <72224428+vinayakbanga@users.noreply.github.com>
2022-06-21 02:20:37 +0530

    Update index.js

--- server/API/Image/index.js
+++ server/API/Image/index.js
@@ -15,15 +15,6 @@ const Router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 
-//aws s3 buket setting up
-// const s3Bucket= new AWS.S3({
-   
-//     // accessKeyId:process.env.AWS_S3_ACCESS_KEY,
-//     // secretAccessKey:process.env.AWS_S3_SECRET_KEY,
-//      accessKeyId:"AKIA6FQECZYODNW7DFTK",
-//     secretAccessKey:"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",
-//     region:"ap-south-1",
-// });
 
 
 /*
